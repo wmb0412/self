56915a93ca5f0cc98e05380b204b4dfd0bdfb2be
wmb0412 <353685720@qq.com>
2023-08-21 17:32:19 +0800

    feat: 添加react-router-dom、ts、vite别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
+
+const getPath = _path => path.resolve(__dirname, _path)
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": getPath('src'),
+      "@pages": getPath('src/pages'),
+      "@assets": getPath('src/assets'),
+      "@utils": getPath('src/utils')
+    }
+  },
   plugins: [react()],
 })
